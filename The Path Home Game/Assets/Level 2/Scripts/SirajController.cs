f17af502d8761d0ed714dc1d20704aacfae46a46
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2025-12-14 00:13:21 +0200

    Update

--- The Path Home Game/Assets/Level 2/Scripts/SirajController.cs
+++ The Path Home Game/Assets/Level 2/Scripts/SirajController.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class SirajController : MonoBehaviour
 {
     public Transform Rin;
-    public Vector3 offset = new Vector3(1.3f, 1.3f, 0f);
-    public float followSpeed = 3f;
+    public Vector2 offset = new Vector2(1.3f, 1.3f);
+    public float followSpeed = 5f;
+
+    private SpriteRenderer sr;
+    private Rigidbody2D RinRb;
 
     private Animator anim;
 
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        RinRb = Rin.GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        Vector3 targetPos = Rin.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+        Vector2 targetPos = (Vector2)Rin.position + offset;
+        transform.position = Vector2.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
 
-        float distance = Vector3.Distance(transform.position, targetPos);
+        float distance = Vector2.Distance(transform.position, targetPos);
         anim.SetBool("isMoving", distance > 0.1f);
+
+        if (RinRb.velocity.x > 0.1f)
+            sr.flipX = false; 
+
+        else if (RinRb.velocity.x < -0.1f)
+            sr.flipX = true;  
     }
 }
